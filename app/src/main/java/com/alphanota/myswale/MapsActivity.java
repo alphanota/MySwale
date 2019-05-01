1ccfcc0a45be35f2e1e51b091c094992d81f6ead
Angel Montero <a.montero07@hotmail.com>
2019-05-01 12:13:42 -0400

    All GI sites are shown as markers in a GMap

--- app/src/main/java/com/alphanota/myswale/MapsActivity.java
+++ app/src/main/java/com/alphanota/myswale/MapsActivity.java
@@ -9,6 +9,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.data.geojson.GeoJsonLayer;
+
+import org.json.JSONException;
+
+import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -42,5 +47,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        try {
+            GeoJsonLayer layer = new GeoJsonLayer(googleMap, R.raw.dep_gi, this);
+            layer.addLayerToMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
